59d7fa6afdc67eb13e04fd2c1983898a6cf8f436
aram1l7 <arammartirosyann16@gmail.com>
2023-01-14 00:31:11 +0400

    Voicemail in progress  remove font awesome

--- js/webphone.js
+++ js/webphone.js
@@ -2,7 +2,23 @@ let classesStr = `items-center text-[#0D0D54] font-bold bg-[#F7F7FB] border-r-4 
 let activeClasses = classesStr.split(" ");
 let subMenuClassesStr = `bg-[#F7F7FB] text-[#0D0D54] active-submenu`;
 let activeSubMenuClasses = subMenuClassesStr.split(" ");
-
+const svgLoader = `<svg
+aria-hidden="true"
+role="status"
+class="w-10 h-10 mr-3 text-[#00A2DD] animate-spin"
+viewBox="0 0 100 101"
+fill="none"
+xmlns="http://www.w3.org/2000/svg"
+>
+<path
+  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+  fill="#E5E7EB"
+/>
+<path
+  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+  fill="currentColor"
+/>
+</svg>`;
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 
@@ -72,7 +88,7 @@ const handleOpenExtensions = () => {
   };
 };
 
-const formatHistoryDate = (date) => {
+const formatHistoryDate = (date, isVoicemail) => {
   let origDate = new Date(date).toLocaleString("en-us", {
     weekday: "short",
     month: "short",
@@ -80,6 +96,17 @@ const formatHistoryDate = (date) => {
     hour: "numeric",
     minute: "numeric",
   });
+  if(isVoicemail){
+    origDate = new Date(date).toLocaleString("en-us", {
+      weekday: "short",
+      month: "2-digit",
+      year:"2-digit",
+      day: "2-digit",
+      hour: "numeric",
+      minute: "numeric",
+    }).replace(/\//g, '-').replace(/,/, '');
+    
+  }
   return origDate;
 };
 
@@ -194,23 +221,7 @@ const openDetailedOptions = async (id) => {
 
   const loaderElement = document.createElement("div");
   loaderElement.id = "log-loader";
-  loaderElement.innerHTML = `<svg
-        aria-hidden="true"
-        role="status"
-        class="w-10 h-10 mr-3 text-[#00A2DD] animate-spin"
-        viewBox="0 0 100 101"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-          fill="#E5E7EB"
-        />
-        <path
-          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-          fill="currentColor"
-        />
-      </svg>`;
+  loaderElement.innerHTML = svgLoader;
 
   if (!document.getElementById("log-loader")) {
     spinnerLoader.insertAdjacentElement("afterbegin", loaderElement);
@@ -238,6 +249,71 @@ const openDetailedOptions = async (id) => {
   spinnerLoader.classList.remove("grid");
 
   drawDetailedLog(data);
+};
+
+
+const openVoicemailDetails = (data, id) => {
+  let voiceMailDetails = document.getElementById("voicemail-details")
+  const activeItem = data.find(item => item._id === id)
+  console.log(activeItem,"activeItem");
+  voiceMailDetails.classList.remove('hidden')
+  voiceMailDetails.classList.add('flex')
+  voiceMailDetails.querySelector("#voicemail-number").innerText = activeItem.extension_source
+  voiceMailDetails.querySelector("#voicemail-message span").innerText = activeItem.source_representation_name
+}
+
+const drawVoicemails = () => {
+  let voiceMailLoader = document.getElementById("voicemail-list-loader");
+  let voiceMailList = document.getElementById("voicemail-list");
+
+  voiceMailLoader.classList.add('hidden')
+  voiceMailLoader.classList.remove('grid')
+
+  const list = localStorage.getItem("voicemails");
+  if (list) {
+    const listData = JSON.parse(list);
+    let html = listData.map(item => {
+      let formatedDate = formatHistoryDate(item.time_received, true);
+
+      return `
+      <div 
+        data-id="${item._id}" 
+        data-listened="${item.listened}"
+        class="flex voicemail-list-item cursor-pointer justify-between select-none px-6 py-2 
+        items-center border-b border-[#D3D3D3]">
+          <div class="flex gap-4 items-start w-full">
+            <div class="w-14 h-14">
+              <img class="rounded-full" src="/images/profile.svg"/>
+            </div>
+            <div class="flex flex-col w-full gap-2">
+              <div class="flex justify-between w-full">
+                <p class="text-[#232323]">${item.extension_source}</p>
+                <div class="w-3.5 h-3.5 rounded-full bg-[#6FC316] ${!item.listened ? "block" : "hidden"}"></div>
+              </div>
+              <div class="flex justify-between w-full">
+                <p class="text-[#4D4D4D]">${item.source_representation_name}</p>
+                <span class="text-sm text-[#939393]">${formatedDate}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      `
+    }).join(" ")
+
+    voiceMailList.classList.remove('hidden')
+    voiceMailList.classList.add('flex')
+
+    voiceMailList.innerHTML = html
+
+    voiceMailList.querySelectorAll(".voicemail-list-item").forEach(item => {
+      item.addEventListener('click', function(e){
+        console.log(this,"this");
+        openVoicemailDetails(listData,this.dataset.id)
+      })
+    })
+  }
+
+
 };
 
 const drawCallHistory = () => {
@@ -340,6 +416,35 @@ async function getCallHistory() {
     if (isLocalhost) {
       localStorage.setItem("call_history", JSON.stringify(mockHistory));
       drawCallHistory();
+    }
+  }
+}
+
+async function getVoicemails() {
+  try {
+    const history = await fetch(
+      `${backendApi}/voicemail/messages?from_date=${
+        beforeHistory.split("T")[0]
+      }&to_date=${
+        new Date().toISOString().split("T")[0]
+      }&extension_destination=${getCookie("user_id")}
+    `,
+      {
+        headers: {
+          Authorization: id_token,
+        },
+      }
+    );
+    if (history.ok) {
+      const data = await history.json();
+      localStorage.setItem("voicemails", JSON.stringify(data));
+      console.log(data, "data");
+      drawVoicemails();
+    }
+  } catch (error) {
+    if (isLocalhost) {
+      localStorage.setItem("voicemails", JSON.stringify(mockHistory));
+      drawVoicemails();
     }
   }
 }
@@ -475,6 +580,19 @@ async function updateUI() {
     };
 
     voiceMailTab.onclick = function () {
+      let voiceMailLoader = document.getElementById("voicemail-list-loader");
+      const loaderElement = document.createElement("div");
+      loaderElement.id = "voicemail-loader";
+      loaderElement.innerHTML = svgLoader;
+    
+      if (!document.getElementById("log-loader")) {
+        voiceMailLoader.insertAdjacentElement("afterbegin", loaderElement);
+      }
+
+      if (this.dataset.shouldFetch !== "false") {
+        getVoicemails();
+        this.dataset.shouldFetch = "false";
+      }
       removeActiveTab();
       setActiveTab(this);
       pageTitle.innerText = "Voicemail";
